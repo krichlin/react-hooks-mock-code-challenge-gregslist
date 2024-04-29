a8637e5a79565e5b0dbc524935d34045539e3d57
Ken Richlin <krichlin@gmail.com>
2024-04-29 11:51:36 -0400

    first objective complete

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function ListingCard() {
+function ListingCard(props) {
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={props.image} alt={"description"} />
       </div>
       <div className="details">
         {true ? (
@@ -13,8 +13,8 @@ function ListingCard() {
         ) : (
           <button className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
+        <strong>{props.description}</strong>
+        <span> · {props.location}</span>
         <button className="emoji-button delete">🗑</button>
       </div>
     </li>
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,32 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useEffect, useState} from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      console.log("data",data)
+      setListings(data)
+    })
+}, [])
+  
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {
+          // console.log(listings)
+          
+          listings.map((listings) => (
+            <ListingCard key={listings.id} description={listings.description} image={listings.image} location={listings.location} />
+          ))
+
+        }
       </ul>
     </main>
   );
